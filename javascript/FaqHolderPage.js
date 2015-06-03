0eb0fda462b9c86186a669ae8131b89d2343a3ac
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2015-06-03 04:49:22 +0000

    JS Enhacements
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/faqs/trunk@6367 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- javascript/FaqHolderPage.js
+++ javascript/FaqHolderPage.js
@@ -32,9 +32,9 @@ var FaqHolderPage = {
 				var answer = parent.find(FaqHolderPage.answersSelector);
 				if(question.hasClass(FaqHolderPage.openQuestion)) {
 					parent.removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion);
-					question.removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion);
-					answer.removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion)
-						.slideUp();
+					//question.removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion);
+					//answer.removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion).slideUp();
+					answer.slideUp();
 				}
 				else {
 					var siblings = parent.siblings("li."+FaqHolderPage.openQuestion);
@@ -43,29 +43,21 @@ var FaqHolderPage = {
 							//remove class from parent
 							jQuery(siblingParent).removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion);
 							// close other questions
-							jQuery(siblingParent).find(FaqHolderPage.questionLinkSelector).removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion);
+							//jQuery(siblingParent).find(FaqHolderPage.questionLinkSelector).removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion);
 							//close other answers
-							jQuery(siblingParent).find(FaqHolderPage.answersSelector).removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion)
-								.slideUp();
+							//jQuery(siblingParent).find(FaqHolderPage.answersSelector).removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion).slideUp();
+							jQuery(siblingParent).find(FaqHolderPage.answersSelector).slideUp();
 						}
 					);
 					parent.removeClass(FaqHolderPage.closedQuestion).addClass(FaqHolderPage.openQuestion);
-					question.removeClass(FaqHolderPage.closedQuestion).addClass(FaqHolderPage.openQuestion);
-					answer.removeClass(FaqHolderPage.closedQuestion).addClass(FaqHolderPage.openQuestion).slideDown();
+					//question.removeClass(FaqHolderPage.closedQuestion).addClass(FaqHolderPage.openQuestion);
+					//answer.removeClass(FaqHolderPage.closedQuestion).addClass(FaqHolderPage.openQuestion).slideDown();
+					answer.slideDown();
 				}
-				//close all questions
-				//jQuery(parent.find(".FaqQuestion")).toggleClass(FaqHolderPage.openQuestion);
-				//toggle current question
-				//question.toggleClass(FaqHolderPage.openQuestion);
-				//answer.toggleClass(FaqHolderPage.closedQuestion).slideToggle();
-				//var currentlyOpened = parent.siblings().find("h3.faqOpenQuestion");
-				//jQuery(currentlyOpened).parents("li").find(".FaqQuestion").toggleClass(FaqHolderPage.openQuestion);
-				//jQuery(currentlyOpened).parents("li").find(FaqHolderPage.answersSelector).toggleClass(FaqHolderPage.closedQuestion).slideToggle();
 			}
 		);
 	}
 }
-//as soon as possible
 
-jQuery(FaqHolderPage.answersSelector).addClass(FaqHolderPage.closeQuestion);
-jQuery(FaqHolderPage.answersLinkSelector).addClass(FaqHolderPage.closeQuestion);
+//as soon as possible
+jQuery(FaqHolderPage.questionSelector).addClass(FaqHolderPage.closeQuestion);
