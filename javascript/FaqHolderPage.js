12157850e15cd5b7c31211f371ae5c50a90b1dd8
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2015-06-03 03:02:25 +0000

    JS fix
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/faqs/trunk@6366 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- javascript/FaqHolderPage.js
+++ javascript/FaqHolderPage.js
@@ -11,29 +11,56 @@
 
 var FaqHolderPage = {
 
-	answersSelector: ".FaqAnswer",
+	questionSelector: ".FaqQuestion",
 
-	answersLinkSelector: ".FaqQuestion a",
+	questionLinkSelector: ".FaqQuestion a",
+
+	answersSelector: ".FaqAnswer",
 
 	openQuestion: "faqOpenQuestion",
 
-	closeQuestion: "faqClosedQuestion",
+	closedQuestion: "faqClosedQuestion",
 
 	init: function() {
 
-		jQuery(FaqHolderPage.answersLinkSelector).on(
+		jQuery(FaqHolderPage.questionLinkSelector).on(
 			"click",
 			function(event) {
 				event.preventDefault();
 				var question = jQuery(this);
 				var parent = jQuery(this).parents("li");
 				var answer = parent.find(FaqHolderPage.answersSelector);
-				question.toggleClass(FaqHolderPage.openQuestion);
-				jQuery(parent.find(".FaqQuestion")).toggleClass(FaqHolderPage.openQuestion);
-				answer.toggleClass(FaqHolderPage.closeQuestion).slideToggle();
-				var currentlyOpened = parent.siblings().find("h3.faqOpenQuestion");
-				jQuery(currentlyOpened).parents("li").find(".FaqQuestion").toggleClass(FaqHolderPage.openQuestion);
-				jQuery(currentlyOpened).parents("li").find(FaqHolderPage.answersSelector).toggleClass(FaqHolderPage.closeQuestion).slideToggle();
+				if(question.hasClass(FaqHolderPage.openQuestion)) {
+					parent.removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion);
+					question.removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion);
+					answer.removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion)
+						.slideUp();
+				}
+				else {
+					var siblings = parent.siblings("li."+FaqHolderPage.openQuestion);
+					jQuery(siblings).each(
+						function(i, siblingParent) {
+							//remove class from parent
+							jQuery(siblingParent).removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion);
+							// close other questions
+							jQuery(siblingParent).find(FaqHolderPage.questionLinkSelector).removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion);
+							//close other answers
+							jQuery(siblingParent).find(FaqHolderPage.answersSelector).removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion)
+								.slideUp();
+						}
+					);
+					parent.removeClass(FaqHolderPage.closedQuestion).addClass(FaqHolderPage.openQuestion);
+					question.removeClass(FaqHolderPage.closedQuestion).addClass(FaqHolderPage.openQuestion);
+					answer.removeClass(FaqHolderPage.closedQuestion).addClass(FaqHolderPage.openQuestion).slideDown();
+				}
+				//close all questions
+				//jQuery(parent.find(".FaqQuestion")).toggleClass(FaqHolderPage.openQuestion);
+				//toggle current question
+				//question.toggleClass(FaqHolderPage.openQuestion);
+				//answer.toggleClass(FaqHolderPage.closedQuestion).slideToggle();
+				//var currentlyOpened = parent.siblings().find("h3.faqOpenQuestion");
+				//jQuery(currentlyOpened).parents("li").find(".FaqQuestion").toggleClass(FaqHolderPage.openQuestion);
+				//jQuery(currentlyOpened).parents("li").find(FaqHolderPage.answersSelector).toggleClass(FaqHolderPage.closedQuestion).slideToggle();
 			}
 		);
 	}
