9ec865cabdb33fd5d4d1a67d9471b3b66c75e0e2
Nicolaas Thiemen Francken @ B&C <github@sunnysideup.co.nz>
2017-05-16 15:41:38 +1200

    MAJOR: template rework

--- javascript/FaqHolderPage.js
+++ javascript/FaqHolderPage.js
@@ -4,59 +4,58 @@
  **/
 
 ;(function($) {
-	$(document).ready(function() {
-		FaqHolderPage.init();
-	});
+    $(document).ready(function() {
+        FaqHolderPage.init();
+    });
 })(jQuery);
 
 var FaqHolderPage = {
 
-	questionSelector: ".FaqQuestion",
+    questionSelector: ".faq-one",
 
-	questionLinkSelector: ".FaqQuestion a",
+    questionLinkSelector: ".faq-one h3",
 
-	answersSelector: ".FaqAnswer",
+    answersSelector: ".faq-answer",
 
-	openQuestion: "faqOpenQuestion",
+    openQuestion: "open",
 
-	closedQuestion: "faqClosedQuestion",
+    closedQuestion: "closed",
 
-	init: function() {
+    init: function() {
 
-		jQuery(FaqHolderPage.questionLinkSelector).on(
-			"click",
-			function(event) {
-				event.preventDefault();
-				var question = jQuery(this);
-				var parent = jQuery(this).parents("li");
-				var answer = parent.find(FaqHolderPage.answersSelector);
-				if(parent.hasClass(FaqHolderPage.openQuestion)) {
-					parent.removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion);
-					//question.removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion);
-					//answer.removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion).slideUp();
-					answer.slideUp();
-				}
-				else {
-					var siblings = parent.siblings("li."+FaqHolderPage.openQuestion);
-					jQuery(siblings).each(
-						function(i, siblingParent) {
-							//remove class from parent
-							jQuery(siblingParent).removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion);
-							// close other questions
-							//jQuery(siblingParent).find(FaqHolderPage.questionLinkSelector).removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion);
-							//close other answers
-							//jQuery(siblingParent).find(FaqHolderPage.answersSelector).removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion).slideUp();
-							jQuery(siblingParent).find(FaqHolderPage.answersSelector).slideUp();
-						}
-					);
-					parent.removeClass(FaqHolderPage.closedQuestion).addClass(FaqHolderPage.openQuestion);
-					//question.removeClass(FaqHolderPage.closedQuestion).addClass(FaqHolderPage.openQuestion);
-					//answer.removeClass(FaqHolderPage.closedQuestion).addClass(FaqHolderPage.openQuestion).slideDown();
-					answer.slideDown();
-				}
-			}
-		);
-	}
+        jQuery(FaqHolderPage.questionLinkSelector).on(
+            "click",
+            function(event) {
+                event.preventDefault();
+                var parent = jQuery(this).closest("li");
+                var answer = parent.find(FaqHolderPage.answersSelector);
+                if(parent.hasClass(FaqHolderPage.openQuestion)) {
+                    parent.removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion);
+                    //question.removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion);
+                    //answer.removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion).slideUp();
+                    answer.slideUp();
+                }
+                else {
+                    var siblings = parent.siblings("li."+FaqHolderPage.openQuestion);
+                    jQuery(siblings).each(
+                        function(i, siblingParent) {
+                            //remove class from parent
+                            jQuery(siblingParent).removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion);
+                            // close other questions
+                            //jQuery(siblingParent).find(FaqHolderPage.questionLinkSelector).removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion);
+                            //close other answers
+                            //jQuery(siblingParent).find(FaqHolderPage.answersSelector).removeClass(FaqHolderPage.openQuestion).addClass(FaqHolderPage.closedQuestion).slideUp();
+                            jQuery(siblingParent).find(FaqHolderPage.answersSelector).slideUp();
+                        }
+                    );
+                    parent.removeClass(FaqHolderPage.closedQuestion).addClass(FaqHolderPage.openQuestion);
+                    //question.removeClass(FaqHolderPage.closedQuestion).addClass(FaqHolderPage.openQuestion);
+                    //answer.removeClass(FaqHolderPage.closedQuestion).addClass(FaqHolderPage.openQuestion).slideDown();
+                    answer.slideDown();
+                }
+            }
+        );
+    }
 }
 
 //as soon as possible
--- templates/Includes/FaqOneQuestion.ss
+++ templates/Includes/FaqOneQuestion.ss
@@ -1,4 +1,4 @@
-<li class="$FirstLast FaqOne">
-	<h3 class="FaqQuestion" id="question-{$URLSegment}"><a href="$Link">$Title</a></h3>
-	<div class="FaqAnswer" id="answer-{$URLSegment}">$Content</div>
+<li class="$FirstLast faq-one">
+    <h3 class="faq-question" id="question-{$ID}"><a href="$Link">$Title</a></h3>
+    <div class="faq-answer" id="answer-{$ID}">$Content</div>
 </li>
