7481e7c8b2ff97b33f35d1125627bd6447635f21
LABCAT <shane.watters.nz@gmail.com>
2020-07-06 12:50:27 +1200

    Improvements as suggested by php-stan.

--- src/FaqHolderPageController.php
+++ src/FaqHolderPageController.php
@@ -13,7 +13,7 @@ class FaqHolderPageController extends PageController
     /**
      * returns all underlying FaqOnePage pages...
      * for use in templates
-     * @return DataList | Null
+     * @return \SilverStripe\ORM\DataList | Null
      */
     public function Entries()
     {
